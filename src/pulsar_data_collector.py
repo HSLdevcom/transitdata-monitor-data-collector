530f5d4a30dc4c62e697b652c41d4edd86eeb2f2
Timo Järvinen <timo.jarvinen@yahoo.com>
2024-12-03 15:26:31 +0200

    Namespace temporarily updated

--- src/pulsar_data_collector.py
+++ src/pulsar_data_collector.py
@@ -119,7 +119,7 @@ def send_pulsar_topic_metric_into_azure(
                 # Metric (name): name of the metric
                 "metric": log_analytics_metric_name,
                 # Namespace: Categorize or group similar metrics together
-                "namespace": "Pulsar",
+                "namespace": "PulsarProd",
                 # Dimension (dimNames): Metric has a single dimension
                 "dimNames": [
                   "Topic"
