d3bee1e9c07a7d277236eb85ae8d5243492fddef
Alex <chihaiaalex@gmail.com>
2025-12-10 00:57:32 +0200

    57139 gtfsrt-metric-exporter

--- transitdata-metrics-exporter/src/main/java/fi/hsl/transitdata/monitoring/HealthHandler.java
+++ transitdata-metrics-exporter/src/main/java/fi/hsl/transitdata/monitoring/HealthHandler.java
@@ -4,7 +4,6 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.io.OutputStream;
 
 class HealthHandler implements HttpHandler {
 
@@ -13,7 +12,7 @@ class HealthHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         exchange.sendResponseHeaders(200, RESPONSE.length);
-        try (OutputStream os = exchange.getResponseBody()) {
+        try (var os = exchange.getResponseBody()) {
             os.write(RESPONSE);
         }
     }
